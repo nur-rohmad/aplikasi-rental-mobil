c50517cfe0f0138888682a029439ac6317014ab5
Nur Rohmad <65668031+nur-rohmad@users.noreply.github.com>
2023-06-21 14:47:57 +0700

    Update Akun.php

--- application/controllers/Akun.php
+++ application/controllers/Akun.php
@@ -7,6 +7,7 @@ class Akun extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+
         $this->load->library('form_validation');
         // library send email
         $this->load->library('email');
